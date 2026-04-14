63722db084e8ba10b25fac3c36149cc7bd58c891
Manikankana <giggyflaggy@gmail.com>
2026-04-14 12:46:22 +0530

    fix cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://nvyc-questions.vercel.app/"
+}));
 app.use(express.json());
 
 app.use("/api/questions", require("./routes/questionRoutes"));
